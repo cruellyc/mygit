9f1b239b046a0d5e2798760ff6c5f5995dfa3e91
cruel <2311499647@qq.com>
2016-05-18 13:51:49 +0800

    修改添加

--- GetOtherClassFiled.java
+++ GetOtherClassFiled.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -27,9 +28,33 @@ public class GetOtherClassFiled {
             Class<?> type = field2[j].getType();  
             System.out.println(prev + " " + type.getName() + " " + field2[j].getName());  
         }
+      //获取方法 
         Method[] m=demo.getMethods();
         for(int i=0;i<m.length;i++){
         	System.out.println(m[i].getName());
         }
+        
+        
+      //获取接口类  
+        Class<?> interfaces[] = demo.getInterfaces();  
+       //获取父类  
+        Class<?> parents = demo.getSuperclass();  
+        //获取所有的构造函数  
+        Constructor<?> constructors[] = demo.getConstructors();  
+        for (int i = 0; i < interfaces.length; i++) {  
+            System.out.println("实现了哪些接口类:" + interfaces[i].getName());  
+        }  
+        for (int i = 0; i < constructors.length; i++) {  
+            System.out.println("类有哪些构造函数:" + constructors[i]);  
+        }  
+        System.out.println("继承的父类:" + parents.getName());  
+        for (int i = 0; i < constructors.length; i++) {  
+          Class<?> paramenter[] = constructors[i].getParameterTypes();  
+          int mo = constructors[i].getModifiers();  
+          System.out.println(Modifier.toString(mo) + " " + constructors[i].getName());  
+          for(int j=0;j<paramenter.length;j++){  
+              System.out.println(paramenter[j].getName());  
+         }  
+         }
      }
 }
