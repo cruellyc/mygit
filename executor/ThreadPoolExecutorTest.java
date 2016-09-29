6ca372925fbbcb314c2ea784b0cb990fc2d87301
cruel <2311499647@qq.com>
2016-09-29 14:43:17 +0800

    线程池的例子

---
+++ executor/ThreadPoolExecutorTest.java
@@ -0,0 +1,111 @@
+package executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author  liyc
+ * @date 2016年9月29日 下午2:28:18
+*/
+public class ThreadPoolExecutorTest {
+	public static void main(String[] args) {
+		//cachedThread();
+		//fixedThread();
+		schedule();
+	}
+	
+	/**
+	 * 一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程
+	 */
+	public static void cachedThread() {
+		ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+		for (int i = 0; i < 10; i++) {
+			final int index = i;
+			try {
+				Thread.sleep(index * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			cachedThreadPool.execute(new Runnable() {
+				public void run() {
+					System.out.println(index);
+				}
+			});
+		}
+	}
+	/**
+	 * 一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待
+	 */
+	public static void fixedThread(){
+		ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+		for (int i = 0; i < 10; i++) {
+			final int index = i;
+			fixedThreadPool.execute(new Runnable() {
+				public void run() {
+					try {
+						System.out.println(index);
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+	}
+	/**
+	 * 一个定长线程池，支持定时及周期性任务执行。延迟,表示延迟3秒执行
+	 */
+	public static void schedule(){
+		ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+		scheduledThreadPool.schedule(new Runnable() {
+			public void run() {
+				System.out.println("delay 3 seconds");
+			}
+		}, 3, TimeUnit.SECONDS);
+	}
+	
+	
+	/**
+	 * 一个定长线程池，支持定时及周期性任务执行。定期执行,表示延迟1秒后每3秒执行一次
+	 */
+	public static void scheduledAtFixedRate() {
+		ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+		scheduledThreadPool.scheduleAtFixedRate(new Runnable() {
+			public void run() {
+				System.out.println("delay 1 seconds, and excute every 3 seconds");
+			}
+		}, 1, 3, TimeUnit.SECONDS);
+	}
+
+	
+	
+	/**
+	 * 创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行
+	 */
+	public static void singleThread(){
+		ExecutorService singleThreadExecutor = Executors.newCachedThreadPool();
+		for (int i = 0; i < 100; i++) {
+			final int index = i;
+			singleThreadExecutor.execute(new Runnable() {
+				public void run() {
+					try {
+						while (true) {
+							System.out.println(index);
+							Thread.sleep(10 * 1000);
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
